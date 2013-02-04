39fa206d4745dc9b1da8d3c43529900f19b408ae
Jorge Moscat <jorgemoscat@gmail.com>
2013-02-03 21:26:04 -0800

    initial page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThanxupWeb::Application.config.secret_token = 'c545255a5b35ec01ce7b1b7cb6494abd439274bef8c2e448424ccc1b958cb5905f174c19cee9eba1928e5ead88608158035074a338662f98cbe5800e902b86c9'
