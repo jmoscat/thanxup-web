6bf79e7390431abdb526c6f5f554164fe6aa00b8
Jorge Moscat <jorgemoscat@gmail.com>
2013-02-18 16:34:39 -0800

    Added hr and email Carlos

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -2,6 +2,6 @@ class QuestionMailer < ActionMailer::Base
  default :from => "jorgemoscat@gmail.com"  
   def question_email(wazzup)
   	@wazzup = wazzup
-    mail(:to => "jorgemoscat@gmail.com", :subject => "Hey Charly, nuevo mensaje de la web")
+    mail(:to => "cestevezm@gmail.com", :subject => "Hey Charly, nuevo mensaje de la web")
   end
 end
